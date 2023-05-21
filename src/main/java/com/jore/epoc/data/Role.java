a50afd2bec0d839bca850bd6bd4df3fa91913ee5
start.vaadin.com <no-reply@vaadin.com>
2023-05-21 13:44:22 +0000

    Generated project

---
+++ src/main/java/com/jore/epoc/data/Role.java
@@ -0,0 +1,5 @@
+package com.jore.epoc.data;
+
+public enum Role {
+    USER, ADMIN;
+}
---
+++ src/main/java/com/jore/epoc/security/AuthenticatedUser.java
@@ -0,0 +1,30 @@
+package com.jore.epoc.security;
+
+import com.jore.epoc.data.entity.User;
+import com.jore.epoc.data.service.UserRepository;
+import com.vaadin.flow.spring.security.AuthenticationContext;
+import java.util.Optional;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthenticatedUser {
+
+    private final UserRepository userRepository;
+    private final AuthenticationContext authenticationContext;
+
+    public AuthenticatedUser(AuthenticationContext authenticationContext, UserRepository userRepository) {
+        this.userRepository = userRepository;
+        this.authenticationContext = authenticationContext;
+    }
+
+    public Optional<User> get() {
+        return authenticationContext.getAuthenticatedUser(UserDetails.class)
+                .map(userDetails -> userRepository.findByUsername(userDetails.getUsername()));
+    }
+
+    public void logout() {
+        authenticationContext.logout();
+    }
+
+}
